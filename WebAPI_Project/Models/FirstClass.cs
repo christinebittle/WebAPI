ec5848d49e133033ecb05d0279560a661657845e
Somebody <christinebitts@gmail.com>
2020-08-24 11:01:49 -0400

    Add project files.

---
+++ WebAPI_Project/Models/FirstClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+
+namespace WebAPI_Project.Models
+{
+    public class FirstClass
+    {
+        //Sets the value of "a" to -10
+        int a = -10;
+        //Sets the value of "b" to 7
+        int b = 7;
+        //Sets the value "c" to 13
+        int c = (10 / 2) + 8;
+        
+        //Sets the value of "d" to "Hello"
+        string d = "Hello";
+
+        //Sets the value of "e" to be "Hello World! It's a nice day!"
+        string e = "Hello " + "World!" + " It's a nice day!";
+    }
+}
